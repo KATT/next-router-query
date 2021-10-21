a7d50ba0745eea83385d5213e6afd4f13b98da47
KATT <alexander@n1s.se>
2021-10-21 23:43:54 +0200

    let's try again

--- example/components/ShowDifference.tsx
+++ example/components/ShowDifference.tsx
@@ -8,14 +8,20 @@ export function ShowDifference() {
     return null;
   }
 
+  console.log({
+    'useRouter().query': nativeQuery,
+    'useRouterQuery()': myQuery,
+  });
+
   return (
     <div>
+      <p>Open inspector to see the difference</p>
       <h2>
         <code>useRouterQuery()</code> output
       </h2>
       <em>
-        If you see flicker here it's probably only the keys re-ordering after
-        Next.js returns with values
+        If you see flicker here it&apos;s probably only the keys re-ordering
+        after Next.js returns with values
       </em>
       <pre>
         <pre>{JSON.stringify(myQuery, null, 4)}</pre>
