b5ca27d2ea5585a1d08191516ac0b31a2b049d31
KATT <alexander@n1s.se>
2021-10-21 23:00:19 +0200

    initial commit

---
+++ src/index.test.tsx
@@ -0,0 +1,43 @@
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+    };
+  },
+}));
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { useRouterQuery } from '.';
+
+jest.mock('next/dist/client/router', () => ({
+  __esModule: true,
+  useRouter: () => ({
+    query: {},
+    pathname: '/',
+    asPath: '/',
+    events: {
+      emit: jest.fn(),
+      on: jest.fn(),
+      off: jest.fn(),
+    },
+    push: jest.fn(() => Promise.resolve(true)),
+    prefetch: jest.fn(() => Promise.resolve(true)),
+    replace: jest.fn(() => Promise.resolve(true)),
+  }),
+}));
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  let res: any;
+  function App() {
+    const query = useRouterQuery();
+    res = query;
+    return null;
+  }
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+
+  expect(res).toMatchInlineSnapshot(`Object {}`);
+});
---
+++ src/index.tsx
@@ -0,0 +1,62 @@
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
+
+/**
+ * Like `useRouter().query` but will return it on first render
+ */
+export function useRouterQuery() {
+  const router = useRouter();
+
+  const { asPath, query, pathname } = router;
+  const value = useMemo(() => {
+    // if we have query params from the router, let's return that
+    if (Object.keys(query).length > 0) {
+      return query;
+    }
+    // split path by `/` or `?`
+    const pathnameParts = pathname.split(/\/|\?/);
+
+    const loc =
+      typeof window !== 'undefined'
+        ? {
+            query: Object.fromEntries(
+              new URLSearchParams(window.location.search)
+            ),
+            pathname: location.pathname,
+          }
+        : {
+            query,
+            pathname: asPath,
+          };
+
+    const asPathParts = loc.pathname.split(/\/|\?/);
+
+    // actual query object that we wanna use
+    const actualQuery: typeof query = {};
+    for (let index = 0; index < pathnameParts.length; index++) {
+      const part = pathnameParts[index];
+      if (!part.startsWith('[') || !part.endsWith(']')) {
+        continue;
+      }
+      // extract real query param from `post/[id]` style routes
+
+      // removes first and last character
+      const key = part.slice(1, -1);
+      if (key.startsWith('...')) {
+        // catch-all route
+        const catchAllKey = key.slice(3);
+        actualQuery[catchAllKey] = asPathParts.splice(index);
+        break;
+      }
+
+      // append to "actual query"
+      actualQuery[key] = asPathParts[index];
+    }
+    for (const key in loc.query) {
+      actualQuery[key] = loc.query[key];
+    }
+    return actualQuery;
+  }, [asPath, query, pathname]);
+
+  return value;
+}
