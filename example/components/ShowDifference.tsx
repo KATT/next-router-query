c4e7e436683b9c37de59747ce18242a17d8f2ef2
KATT <alexander@n1s.se>
2021-10-21 23:38:40 +0200

    example

---
+++ example/components/ShowDifference.tsx
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router';
+import { useRouterQuery } from 'next-router-query';
+
+export function ShowDifference() {
+  const nativeQuery = useRouter().query;
+  const myQuery = useRouterQuery();
+
+  return (
+    <div>
+      <h2>
+        <code>useRouterQuery()</code> output
+      </h2>
+      <em>
+        If you see flicker here it's probably only the keys re-ordering after
+        Next.js returns with values
+      </em>
+      <pre>
+        <pre>{JSON.stringify(myQuery, null, 4)}</pre>
+      </pre>
+      <h2>
+        <code>useRouter().query</code> output
+      </h2>
+      <pre>
+        <em>Reload and notice the slight flickering below</em>
+        <pre>{JSON.stringify(nativeQuery, null, 4)}</pre>
+      </pre>
+    </div>
+  );
+}
--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,11 +1,36 @@
+import Link from 'next/link';
+
 export default function IndexPage() {
   return (
     <>
       <ul>
         <li>
-          <a href="/post/myId">
-            <code>post/[id].tsx</code>
-          </a>
+          <Link href="/post/myId">
+            <a>
+              <code>post/[id].tsx</code>
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={{
+              pathname: 'post/myId',
+              query: {
+                key: 'value',
+              },
+            }}
+          >
+            <a>
+              <code>post/[id].tsx</code> with a <code>?search</code>-param
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/pokemon/gotta/catch/them/all">
+            <a>
+              <code>pokemon/[...all].tsx</code>
+            </a>
+          </Link>
         </li>
       </ul>
     </>
